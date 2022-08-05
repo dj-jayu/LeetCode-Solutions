9de1be2f03e5fe27de4b3b47fdd74035ff3c61fc
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2022-08-04 23:07:35 -0300

    Time: 17 ms (36.70%), Space: 42.3 MB (91.23%) - LeetHub

---
+++ 205-isomorphic-strings/Solution-509.java
@@ -0,0 +1,53 @@
+
+// Better best case
+// We could get a better best case translating both strings in paralel, and returning false on the first mismatch
+// The space complexity is the same, but we would create 2 maps at same time, instead of one then the other in the solution above
+// Time: O(n) -> we iterate through the characters of both strings one time
+// Space: O(n) -> 2*O(1) for the map (max length is number of ASCII characters) + two new strings with same length as originals
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        // We create 2 new strings
+        // coded in the 'first index language'
+        // It work like this:
+        // Every character is translated
+        // to the index of the first appearance
+        // of this character in the string
+        // In the end we compare the translated strings
+        // If they are equal, they are isomorphic
+        
+        // Isomorphic strings need to have the same size
+        if (s.length() != t.length()) return false;
+        
+        Map<Character, Integer> firstIndexMap = new HashMap<Character, Integer>();
+        Map<Character, Integer> secondIndexMap = new HashMap<Character, Integer>();
+        
+        StringBuilder builderOne = new StringBuilder();
+        StringBuilder builderTwo = new StringBuilder();
+        
+        char currentFirstChar;
+        char currentSecondChar;
+        
+        for (int i = 0; i < s.length(); i++) {
+            currentFirstChar = s.charAt(i);
+            currentSecondChar = t.charAt(i);
+            
+            if (!firstIndexMap.containsKey(currentFirstChar)) {
+                firstIndexMap.put(currentFirstChar, i);
+            }
+            
+            if (!secondIndexMap.containsKey(currentSecondChar)) {
+                secondIndexMap.put(currentSecondChar, i);
+            }
+            
+            
+            // for every character translated, we return false if there is a mismatch
+            if (firstIndexMap.get(currentFirstChar) != secondIndexMap.get(currentSecondChar)) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+
+}
