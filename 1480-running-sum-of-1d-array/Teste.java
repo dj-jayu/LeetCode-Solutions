ac2c78e8487f17cd3a1b7b41324857d123f0fb69
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2022-08-05 00:14:10 -0300

    Time: 0 ms (100.00%), Space: 43.8 MB (13.98%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/Teste.java
@@ -0,0 +1,72 @@
+// name: Teste.java
+// Nested loops and external array
+// Time: Big(o) n2 (nested loop)
+// Space: Big(o) n (same size as input array)
+
+// class Solution {
+//     public int[] runningSum(int[] nums) {
+//         // we will create 2 loops to make the sums
+//         // saving the results to a new array which we will return in the end
+        
+//         int[] runningSumArray = new int[nums.length];
+        
+//         for (int stopAtInclusive = 0; stopAtInclusive < nums.length; stopAtInclusive++) {
+//             for (int i = 0; i <= stopAtInclusive; i++) {
+//                 runningSumArray[stopAtInclusive] += nums[i];
+//             }
+//         }
+        
+//         return runningSumArray;
+//     }
+// }
+
+
+// 2-
+// One loop, external array
+// Time: Big(o) n
+// Space: Big(o) n
+
+// class Solution {
+//     public int[] runningSum(int[] nums) {
+//         // we will sum walk through the array
+//         // summing the current elements with the sum
+//         // of the elements before
+//         // saving the result in an external array
+        
+//         int[] runningSumArray = new int[nums.length];
+        
+//         int sumBeforeMe = 0;
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             if (i != 0) sumBeforeMe = runningSumArray[i - 1];
+            
+//             runningSumArray[i] = sumBeforeMe + nums[i];
+//         }
+        
+//         return runningSumArray;
+//     }
+// }
+
+
+// 3-
+// One loop, same array
+// Time: Big(o) n (one pass through the array)
+// Space: Big(o) 1
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        // We walk through the array
+        // summing the current element with the sum of the elements before
+        // we consider the 'zeroth' sum to be 0
+        
+        int sumBeforeMe = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0) sumBeforeMe = nums[i - 1];
+            nums[i] = sumBeforeMe + nums[i];
+            
+        }
+        
+        return nums;
+    }
+}
